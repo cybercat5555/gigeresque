20feff2bfaecadd7c11de2208153afff3da34cf0
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-10-01 01:02:11 -0400

    Update SittingIdolRender.java

--- common/src/main/java/mods/cybercat/gigeresque/client/entity/render/blocks/SittingIdolRender.java
+++ common/src/main/java/mods/cybercat/gigeresque/client/entity/render/blocks/SittingIdolRender.java
@@ -6,11 +6,16 @@ import mod.azure.azurelib.common.api.client.renderer.GeoBlockRenderer;
 import mod.azure.azurelib.common.api.client.renderer.layer.BlockAndItemGeoLayer;
 import mod.azure.azurelib.common.internal.common.cache.object.GeoBone;
 import mods.cybercat.gigeresque.client.entity.model.blocks.SittingIdolModel;
+import mods.cybercat.gigeresque.common.block.GigBlocks;
 import mods.cybercat.gigeresque.common.block.entity.IdolStorageEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.phys.AABB;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class SittingIdolRender extends GeoBlockRenderer<IdolStorageEntity> {
@@ -40,4 +45,32 @@ public class SittingIdolRender extends GeoBlockRenderer<IdolStorageEntity> {
         });
     }
 
+    @Override
+    public void render(IdolStorageEntity animatable, float partialTick, @NotNull PoseStack poseStack, @NotNull MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
+        BlockPos entityPos = animatable.getBlockPos();
+        int searchRadius = 2;
+
+        BlockPos targetBlockPos = null;
+        for (int x = -searchRadius; x <= searchRadius; x++) {
+            for (int y = -searchRadius; y <= searchRadius; y++) {
+                for (int z = -searchRadius; z <= searchRadius; z++) {
+                    BlockPos checkPos = entityPos.offset(x, y, z);
+                    var blockState = animatable.getLevel().getBlockState(checkPos);
+                    if (blockState.is(GigBlocks.ALIEN_STORAGE_BLOCK_INVIS2.get())) {
+                        targetBlockPos = checkPos;
+                        break;
+                    }
+                }
+                if (targetBlockPos != null) break;
+            }
+            if (targetBlockPos != null) break;
+        }
+        if (targetBlockPos != null) {
+            double dx = targetBlockPos.getX() - entityPos.getX();
+            double dz = targetBlockPos.getZ() - entityPos.getZ();
+            float yaw = (float) (Math.atan2(dz, dx) * (180 / Math.PI)) - 90.0F;
+            poseStack.mulPose(Axis.YP.rotationDegrees(yaw));
+        }
+        super.render(animatable, partialTick, poseStack, bufferSource, packedLight, packedOverlay);
+    }
 }
